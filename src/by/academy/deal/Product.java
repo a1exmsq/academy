3dab94a409a8acb28e4bd34b0915febf9f7e492f
a1exmsq <al2003d@gmail.com>
2022-12-20 17:00:52 +0300

    first commit

---
+++ src/by/academy/deal/Product.java
@@ -0,0 +1,49 @@
+package by.academy.deal;
+
+public class Product {
+
+    private String name;
+
+    private int quantity;
+
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    double calcPrice (double price, int quantity) {
+        return price * quantity;
+    }
+
+
+}
